f5d8c83acce7d2de00f564d6a021a849c1f79e42
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-06-21 00:25:46 +0530

    setup done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, make_response
 
 # initialize the app. creating an instance of Flask class
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 # create a root route
 @app.route('/')
@@ -10,10 +11,16 @@ def hello_world():
 
 @app.route('/data')
 def get_data():
-    return jsonify([{
-      'name': 'abhijit',
-      'id': 1
-    }])
+    return jsonify(
+      [{
+        'name': 'Abhijit',
+        'id': 1
+      }, 
+      {
+        'name': 'Padmini',
+        'id': 2
+      }
+      ])
 
 app.run(port=5000,   # run the webserver or app on port 5000
-        debug=True) # set debug = True when you are in development environment
+        debug=True) # set debug = True when you are in development environment, to see updated changes without restarting server
