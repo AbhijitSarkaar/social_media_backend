ab6243cf389cbd095b2cb3af02c11901aeaf7f05
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-07-03 00:47:52 +0530

    GET request with proper response format

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response, request
 from flask_mysqldb import MySQL
+from utils import user_util
 
 # initialize the app. creating an instance of Flask class
 app = Flask(__name__)
@@ -23,8 +24,23 @@ def users():
   resultValue = cur.execute("SELECT * FROM users")
   if resultValue > 0:
     userDetails = cur.fetchall()
+    rows = []
+    for row in userDetails:
+      rows.append(user_util(row))
+    return jsonify(rows)
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+  if request.method == 'POST':
+    userDetails = request.form.to_dict()
     print(userDetails)
-    return jsonify(userDetails)
+    # first_name = userDetails['first_name']
+    # last_name = userDetails['last_name']
+    # cur = mysql.connection.cursor()
+    # cur.execute("INSERT INTO users (first_name, last_name) VALUES (%s, %s)", (first_name, last_name))
+    # mysql.connection.commit()
+    # cur.close()
+    return {'status': 200, 'message': 'Inserted Successfully'}
 
 @app.route('/data')
 def get_data():
@@ -38,6 +54,11 @@ def get_data():
         'id': 2
       }
       ])
+
+
+
+
+
 
 app.run(port=5000,   # run the webserver or app on port 5000
         debug=True) # set debug = True when you are in development environment, to see updated changes without restarting server
---
+++ utils.py
@@ -0,0 +1,7 @@
+def user_util(row):
+  return {
+    "id": row[0],
+    "first_name": row[1],
+    "last_name": row[2]
+  }  
+
